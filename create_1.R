1ac9a3de7b9987b2e5acdc551d946b4c063c603c
InonBercu <inonb@post.bgu.ac.il>
2017-04-04 20:08:18 +0300

    created anither file to ignor and one graph and updated my R script.

--- create_1.R
+++ create_1.R
@@ -1,6 +1,12 @@
 dir.create("git_ignor")
-write.csv("try to ignor me", file = "./git_ignor/git_ignor.csv")
 
-dir.create("git_try")
-write.csv("Show Me What You Got", file = "./git_try/try_me.csv")
+year <- c(2000:2010)
+data_1 <- c(10000,12000,30000,5000,70000,9000,5600,80000,9000,8958,10000)
+
+table_try <- data.frame(year,data_1)
+table_try
+
+plot(year, data_1, col = "blue", pch = 3, title("my_graph"), 
+     ylab = "year", xlab = "data")
+
 
