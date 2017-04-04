0d7c505909720ff4ab81df294eb9fdefa68eac35
InonBercu <inonb@post.bgu.ac.il>
2017-04-04 19:12:42 +0300

    r_project_test

---
+++ create_1.R
@@ -0,0 +1,6 @@
+dir.create("git_ignor")
+write.csv("try to ignor me", file = "./git_ignor/git_ignor.csv")
+
+dir.create("git_try")
+write.csv("Show Me What You Got", file = "./git_try/try_me.csv")
+
